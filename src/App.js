e3284a99a376e0c5a96a38014b6f74014be4073c
Bohdan Kravchenko <bogdankravchenko1997@gmail.com>
2021-07-23 18:13:30 +0300

    Change components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Todo from "./components/todo";
+import Todo from "./components/Todo";
 import './App.css';
-import Header from "./components/header";
+import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import Info from "./components/Info";
 
